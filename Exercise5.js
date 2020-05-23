5f48dc10c25df04f8dbc2e26331ea8556a0dd069
sharjeelyunus <sharjeel924@gmail.com>
2020-05-24 02:47:48 +0500

    Exercise 5

---
+++ Exercise5.js
@@ -0,0 +1,52 @@
+// Make an keyless car EVEN BETTER!
+// We are improving our car from previous exercise now. 
+
+
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+	alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+	alert("Powering On. Enjoy the ride!");
+} else if (Number(age) === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+
+//1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
+function checkDriverAge() {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+
+//2. Create another function that does the same thing, assign it to checkDriverAge2 variable using Function Expression.
+var checkDriverAge2 = function() {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
+// checkDriverAge(92);
+// it returns "Powering On. Enjoy the ride!" 
+function checkDriverAge(age) {
+	if (Number(age) < 18) {
+		return "Sorry, you are too yound to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering On. Enjoy the ride!";
+	} else if (Number(age) === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
