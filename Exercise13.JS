f57f1c35b9f50b72a78e7152314ef58b994e49ab
sharjeelyunus <sharjeel924@gmail.com>
2020-08-21 23:33:02 +0500

    Exercise13

---
+++ Exercise13.JS
@@ -0,0 +1,33 @@
+//Evaluate these:
+//#1
+[2] === [2] // false
+{} === {} // false
+
+//#2 what is the value of property a for each object.
+const object1 = { a: 5 }; // 4
+const object2 = object1; // 4
+const object3 = object2; // 4
+const object4 = { a: 5}; // 5
+object1.a = 4;
+
+
+//#3 create two classes: an Animal class and a Mamal class. 
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+class Animal {
+	constructor(name, type, color) {
+		this.name = name;
+		this.color = color;
+		this.type = type;
+	}
+}
+
+class Mamal extends Animal {
+	constructor(name, type, color) {
+		super(name, type, color)
+	}
+	sound() {
+		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
+	}
+}
+
+const cow = new Mamal('Shelly', 'cow', 'brown');
